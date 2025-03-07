b53e347bbd697125a950997f9bf96d4164a8a570
Liu Jun Yoon <s2680414@landonia20.inf.ed.ac.uk>
2025-03-07 22:21:22 +0000

    update

--- run_experiment.sh
+++ run_experiment.sh
@@ -4,5 +4,5 @@ echo "Running training script"
 python src/main.py --config_dir example_config.yaml
 
 echo "Running test script"
-python src/test.py --task_name med_mcqa --train_size 70 --eval_size 50 --test_size 5 --seed 42 --base_model_type "openbiollm_8b" --base_model_name 'Bio-Medical-Llama-3-2-1B-CoT-012025' --data_dir "MedMCQA"
+python src/test.py --task_name med_mcqa --train_size 70 --eval_size 50 --test_size 150 --seed 42 --base_model_type "openbiollm_8b" --base_model_name 'Bio-Medical-Llama-3-2-1B-CoT-012025' --data_dir "MedMCQA"
 
